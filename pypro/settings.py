018856b03bbdb668148af0a0cb4adec934699405
Hixian <hixian@gmail.com>
2020-10-26 21:41:31 -0300

    Desacopla as configurações para o arquivo .env usando a lib python-decouple

--- pypro/settings.py
+++ pypro/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'q1d4e_jjyzc$&w)_-d(#eigr_i8l1$8^%$&#hl&8&r2_zh%m$w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
