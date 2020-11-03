9c4f43a96cd850ff570e82d63d4088d6ba167629
Hixian <hixian@gmail.com>
2020-11-03 20:20:10 -0300

    Migrações e Customização de Usuário
     close #34

--- pypro/settings.py
+++ pypro/settings.py
@@ -30,6 +30,8 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())  # ['pythonprodjangofr.herokuapp.com']
 
+AUTH_USER_MODEL = 'base.User'
+
 # Application definition
 
 STATICFILES_STORAGE = "django.core.files.storage.FileSystemStorage"
