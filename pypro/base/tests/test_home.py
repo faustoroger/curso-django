808d60042aa6d28c264fdafe5ee1c34301361f4a
Hixian <hixian@gmail.com>
2020-11-10 17:20:17 -0300

    Reorganiza o código de acordo com o flake8
     close #51

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,9 +1,11 @@
 from django.test import Client
 from pypro.django_assertions import assert_contains
 
+
 def test_status_code(client: Client):
     resp = client.get('/')
     assert resp.status_code == 200
+
 
 def test_title(client: Client):
     resp = client.get('/')
