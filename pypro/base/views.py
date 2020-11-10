e2306eeeb6c38bf1d200a767319ed29d856379c1
Hixian <hixian@gmail.com>
2020-11-10 02:04:19 -0300

    Instalar Arquivos Estáticos Localmente
    close #45

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,13 +1,8 @@
-from django.http import HttpResponse
-
-
-# from django.shortcuts import render
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<html> \
-                        <meta charset="UTF-8"> \
-                        <body>Olá, Django!</body></html>',
-                        content_type='text/html')
+    return render(request, 'base/home.html')
