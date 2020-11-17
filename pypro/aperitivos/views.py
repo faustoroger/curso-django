be5757d3724307015b4fd09925788aa5bff9d054
Hixian <hixian@gmail.com>
2020-11-16 22:23:35 -0300

    Implementa templates
     close #72

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 478778775}
+    return render(request, 'aperitivos/video.html', context={'video': video})
